392dfb1a1eee82af7da2ad6261b1f328af384663
G00419446@atu.ie <jseeruthen26@outlook.coom>
2024-11-05 10:06:39 +0000

    car rental project

---
+++ src/test/java/ie/atu/car_rental/CarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.car_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
